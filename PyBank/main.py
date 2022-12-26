6bcc820e5befd979873c5250c3d8cf58254bf048
Crystal Wang <crystalynwang@gmail.com>
2022-12-26 12:42:02 -0800

    update

--- PyBank/main.py
+++ PyBank/main.py
@@ -18,7 +18,7 @@ greatest_decrease = ["", 9999999]
 greatest_increase = ["", 0]
 profit_change_list = []
 profit_average = 0
-date = []
+
 
 
 #open the csv file using CSV module
@@ -27,17 +27,24 @@ with open(csvpath) as csvfile:
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
 
-
+# enter loop
     for row in csvreader:
-
+#calculate total months and net total profit
         total_months += 1
         net_total_profit = net_total_profit + int(row[1])
  
+ # calculate average change (can't get the correct answer)   
+        profit_change_list.append(float(row[1]))
+        if total_months >1:
+                change_months +=1
+                profit_change = profit_change + (profit_change_list[total_months-1]- profit_change_list[total_months -2])
     
+                profit_average = profit_change / change_months  
+# calculate greatest increse and decrese in profits
         profit_change = int(row[1])- previous_profit
         previous_profit = int(row[1])
         profit_change_list = profit_change_list + [profit_change]
-            
+
         if profit_change > greatest_increase [1]:
                 greatest_increase[1]= profit_change
                 greatest_increase[0] = row [0]
@@ -45,11 +52,6 @@ with open(csvpath) as csvfile:
                 greatest_decrease[1]= profit_change
                 greatest_decrease[0] = row [0]
 
-        while (total_months >1):
-                change_months +=1
-                profit_change = profit_change + (profit_change_list[total_months-1]- profit_change_list[total_months -2])
-    
-                profit_average = profit_change / change_months
 
 with open(text_file, 'w') as file:
     file.write("Financial Analysis\n")
